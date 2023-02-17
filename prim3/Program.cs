76a91791c61a09d1e2b520304bd354d57ca7341d
Elena Dabatovka <dabatovka@gmail.com>
2023-02-17 20:21:34 +0300

    3 commit

---
+++ prim3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Написать программу, которая на принимает на вход два числа и проверяет,
+//является ли одно число квадратом другого.
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+if (а * а == b || Math.Pow(b, 2)== a)
+{
+    Console.WriteLine("Да");
+
+}
+else
+{
+    Console.WriteLine("Нет");
+}
