c1c118f39cc35de730cbe8edf761a828b7ea73f6
Elena Dabatovka <dabatovka@gmail.com>
2023-02-17 12:42:05 +0300

    2 Commit

---
+++ prim2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Написать программу, которая принимает на вход число и проверяет 
+//кратно ли оно одновременно 7 и 23.
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine ("Кратно");
+}else{
+    Console.WriteLine ("Не кратное");
+}
