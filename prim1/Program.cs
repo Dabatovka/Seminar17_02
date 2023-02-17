1140122e2795ef1fbc7c6fc9baa58392cf916d18
Elena Dabatovka <dabatovka@gmail.com>
2023-02-17 11:36:00 +0300

    1 commit

---
+++ prim1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Написать прог. которая будет принимать на вход два числа,
+// и выводить является ли второе число кратное первлму,
+// если не кратное то выводить остаток от деления.
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введитк второк число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 % num2 == 0){
+    Console.WriteLine("кратно");}
+else{
+    Console.WriteLine($"не кратно, остаток {num1 % num2}");
+}
